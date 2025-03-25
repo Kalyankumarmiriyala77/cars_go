7cb5b09a0832eecd9fb82079d488ade73f4e56d1
kalyan <kalya@gmail.com>
2025-03-25 23:48:30 +0530

    adding routers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Import BrowserRouter
+//import { BrowserRouter } from 'react-router-dom'; // ✅ Import BrowserRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
 
