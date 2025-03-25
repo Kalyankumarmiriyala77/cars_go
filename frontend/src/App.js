b2206232033c545dcf8c4b5b778540ecceb0b230
kalyan <kalya@gmail.com>
2025-03-25 22:06:20 +0530

    hi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<UserDashboard />} />
-    </Routes>
+    </Routes> 
     </BrowserRouter>
   );
 };
