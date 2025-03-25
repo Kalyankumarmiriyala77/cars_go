536bce272d186f070869c3a617388fb78c8f97d9
kalyan <kalya@gmail.com>
2025-03-25 22:23:59 +0530

    adding Hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { Routes, Route, HashRouter, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter} from 'react-router-dom';
 import IndexPage from './components/IndexPage';
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -8,12 +8,14 @@ import UserDashboard from "./components/UserDashboard";
 
 const App = () => {
   return (
+    <HashRouter>
     <Routes>
       <Route path="/" element={<IndexPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<UserDashboard />} />
     </Routes> 
+    </HashRouter>
   );
 };
 
